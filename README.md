c3f72c7408102989b7a5e8906e197356e6a1cb86
Erik Friese <e.friese0@gmail.com>
2023-07-11 23:37:27 +0200

    Initial crate

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# pyo3-arraylike
+# PyO3-ArrayLike
+
+This crate provides a single struct `PyArrayLike<T,D>` which can be used for extracting an array from any Python object which can be regarded as an array of type `T` and dimension `D` in a reasonable way.
+
+## How it works
+
+`PyArrayLike<T,D>` is basically an enum consisting of either a `PyReadonlyArray<T,D>` from the [rust-numpy](https://github.com/PyO3/rust-numpy) project (i.e. a readonly reference to an actual numpy array on the Python heap), or of an owned `Array<T,D>` from the [ndarray](https://github.com/rust-ndarray/ndarray) project. When calling `pyobject.extract::<PyArrayLike<T,D>>()` it immediately checks whether `pyobject` is already a numpy array of the right type and dimension by calling `pyobject.downcast::<PyReadonlyArray<T,D>>()` internally. Only if this fails, it will try to construct an owned array from `pyobject` by iterating over its elements and trying to convert them into the right type recursively.
+
+## Example
+
+Consider the following function. It takes anything which "looks like" an array of dimension two consisting of integers bewteen 0 and 4294967295. It computes the sum of the array's rows and returns a one dimensional numpy array.
+
+```rust
+#[pyfunction]
+fn sum_of_rows<'py>(py: Python<'py>, ar: PyArrayLike2<'py, u32>) -> &'py PyArray1<u32> {
+    ar.view().sum_axis(Axis(0)).into_pyarray(py)
+}
+```
+
+You can call this function from Python in the following ways. The first two functions will succeed, the latter two will raise an error.
+
+
+```python
+def call1():
+    """Succeeds by passing a reference of the input array to the rust component."""
+    return sum_of_rows(np.array([[1,2,3],[4,5,6]], dtype='uint32'))
+
+def call2():
+    """Succeeds but allocates extra memory for rebuilding the input array inside the rust component."""
+    return sum_of_rows([[1,2,3], iter([4,5,6]), np.array([7,8,9], dtype='int64')])
+
+def call3():
+    """Raises an error since the input array is expected to be two dimensional."""
+    return sum_of_rows([1,2,3])
+
+def call4():
+    """Raises an error since the input array contains a value which cannot be safely casted to u32."""
+    return sum_of_rows([[2**32,0,0], [0,0,0]])
+```
---
+++ src/lib.rs
@@ -0,0 +1,167 @@
+//! This crate provides a single struct `PyArrayLike<T,D>` which can be used for extracting an array from any Python object which can be regarded as an array of type `T` and dimension `D` in a reasonable way.
+
+#![deny(missing_docs, missing_debug_implementations)]
+
+#[cfg(test)]
+mod test;
+
+use ndarray::{Array, ArrayView, Axis, Ix0, Ix1, Ix2, Ix3, Ix4, Ix5, Ix6, IxDyn};
+use numpy::{
+    ndarray::Dimension,
+    pyo3::{exceptions::PyValueError, FromPyObject, PyAny, PyResult, Python},
+    Element, IntoPyArray, PyArray, PyReadonlyArray,
+};
+use std::fmt::Debug;
+
+/// To be used for extracting an array from any Python object which can be regarded as an array of type `T` and dimension `D` in a reasonable way.
+#[derive(Debug)]
+pub struct PyArrayLike<'py, T, D>(ArrayLike<'py, T, D>)
+where
+    T: Element,
+    D: Dimension;
+
+enum ArrayLike<'py, T, D>
+where
+    T: Element,
+    D: Dimension,
+{
+    PyRef(PyReadonlyArray<'py, T, D>),
+    Owned(Array<T, D>, Python<'py>),
+}
+
+impl<'py, T, D> Debug for ArrayLike<'py, T, D>
+where
+    T: Element + Debug,
+    D: Dimension,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::PyRef(py_array) => f.debug_tuple("PyRef").field(py_array).finish(),
+            Self::Owned(array, _) => f.debug_tuple("Owned").field(array).finish(),
+        }
+    }
+}
+
+impl<'py, T, D> PyArrayLike<'py, T, D>
+where
+    T: Element,
+    D: Dimension,
+{
+    /// Consumes `self` and moves its data into an owned array.
+    pub fn into_owned_array(self) -> Array<T, D> {
+        match self.0 {
+            ArrayLike::PyRef(py_array) => py_array.to_owned_array(),
+            ArrayLike::Owned(array, _) => array,
+        }
+    }
+
+    /// Consumes `self` and moves its data into a numpy array.
+    pub fn into_pyarray(self) -> PyReadonlyArray<'py, T, D> {
+        match self.0 {
+            ArrayLike::PyRef(py_array) => py_array,
+            ArrayLike::Owned(array, py) => array.into_pyarray(py).readonly(),
+        }
+    }
+
+    /// Return a read-only view of the array.
+    pub fn view(&self) -> ArrayView<T, D> {
+        match &self.0 {
+            ArrayLike::PyRef(py_array) => py_array.as_array(),
+            ArrayLike::Owned(array, _) => array.view(),
+        }
+    }
+}
+
+impl<'py, T, D> From<PyArrayLike<'py, T, D>> for PyReadonlyArray<'py, T, D>
+where
+    T: Element,
+    D: Dimension,
+{
+    fn from(value: PyArrayLike<'py, T, D>) -> Self {
+        value.into_pyarray()
+    }
+}
+
+impl<T, D> From<PyArrayLike<'_, T, D>> for Array<T, D>
+where
+    T: Element,
+    D: Dimension,
+{
+    fn from(value: PyArrayLike<T, D>) -> Self {
+        value.into_owned_array()
+    }
+}
+
+impl<'py, T, D> PyArrayLike<'py, T, D>
+where
+    T: Element + FromPyObject<'py>,
+    D: Dimension,
+{
+    fn from_python(ob: &'py PyAny) -> Option<Self> {
+        if let Ok(array) = ob.downcast::<PyArray<T, D>>() {
+            return Some(PyArrayLike(ArrayLike::PyRef(array.readonly())));
+        }
+
+        if matches!(D::NDIM, None | Some(0)) {
+            if let Ok(value) = ob.extract::<T>() {
+                let res = Array::from_elem((), value).into_dimensionality().ok()?;
+                return Some(PyArrayLike(ArrayLike::Owned(res, ob.py())));
+            }
+        }
+
+        if matches!(D::NDIM, None | Some(1)) {
+            if let Ok(array) = ob.extract::<Vec<T>>() {
+                let res = Array::from_vec(array).into_dimensionality().ok()?;
+                return Some(PyArrayLike(ArrayLike::Owned(res, ob.py())));
+            }
+        }
+
+        let sub_arrays = ob
+            .iter()
+            .ok()?
+            .map(|item| {
+                item.ok()
+                    .and_then(<PyArrayLike<T, D::Smaller>>::from_python)
+            })
+            .collect::<Option<Vec<_>>>()?;
+        let sub_array_views = sub_arrays.iter().map(|x| x.view()).collect::<Vec<_>>();
+        let array = ndarray::stack(Axis(0), &sub_array_views)
+            .ok()?
+            .into_dimensionality()
+            .ok()?;
+        return Some(PyArrayLike(ArrayLike::Owned(array, ob.py())));
+    }
+}
+
+impl<'py, T, D> FromPyObject<'py> for PyArrayLike<'py, T, D>
+where
+    T: Element + FromPyObject<'py>,
+    D: Dimension,
+{
+    fn extract(ob: &'py PyAny) -> PyResult<Self> {
+        Self::from_python(ob).ok_or_else(|| {
+            let dtype = T::get_dtype(ob.py());
+            let err_text = match D::NDIM {
+                Some(dim) => format!("Expected an array like of dimension {} containing elements which can be safely casted to {}.", dim, dtype),
+                None => format!("Expected an array like of arbitrary dimension containing elements which can be safely casted to {}.", dtype)
+            };
+            PyValueError::new_err(err_text)})
+    }
+}
+
+/// Zero-dimensional array like.
+pub type PyArrayLike0<'py, T> = PyArrayLike<'py, T, Ix0>;
+/// One-dimensional array like.
+pub type PyArrayLike1<'py, T> = PyArrayLike<'py, T, Ix1>;
+/// Two-dimensional array like.
+pub type PyArrayLike2<'py, T> = PyArrayLike<'py, T, Ix2>;
+/// Three-dimensional array like.
+pub type PyArrayLike3<'py, T> = PyArrayLike<'py, T, Ix3>;
+/// Four-dimensional array like.
+pub type PyArrayLike4<'py, T> = PyArrayLike<'py, T, Ix4>;
+/// Five-dimensional array like.
+pub type PyArrayLike5<'py, T> = PyArrayLike<'py, T, Ix5>;
+/// Six-dimensional array like.
+pub type PyArrayLike6<'py, T> = PyArrayLike<'py, T, Ix6>;
+/// Array like of any dimension.
+pub type PyArrayLikeDyn<'py, T> = PyArrayLike<'py, T, IxDyn>;
